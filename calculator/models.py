2bd901aa75c98c9ff9168af85033c577a2170946
Cathy Chen <cathy.chen@airbnb.com>
2016-08-10 20:21:48 -0700

    all pyc files removed

--- calculator/models.py
+++ calculator/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+class Zipcode(models.Model):
+  zipcode = models.IntegerField(null=False, blank=False, unique=True)
+  cbsa = models.IntegerField(null=False, blank=False)
+  cbsa_title = models.CharField(null=False, blank=False, max_length=255)
+  monthly_rent = models.FloatField()
+  nightly_rev = models.FloatField()
+  payoff_nights = models.FloatField()
