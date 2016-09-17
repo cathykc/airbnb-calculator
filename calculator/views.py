83c53e2c90ef59ff6181bd5a0c1c06f176970317
Martin Daniel <martin.daniel@airbnb.com>
2016-09-16 18:08:41 -0700

    edit controller

--- calculator/views.py
+++ calculator/views.py
@@ -8,6 +8,7 @@ from .models import *
 def index(request):
     # limit to dc in the first pass
     zipcode_boundary_points = ZipcodeBoundaryPoint.objects.filter(zipcode__in=range(20002,20038)).all()
+    #zipcode_boundary_points = ZipcodeBoundaryPoint.objects.all()
     zipcode_bounds = {}
 
     for point in zipcode_boundary_points:
@@ -21,18 +22,36 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+def check_zipcode_exist(request):
+    if request.method == "GET":
+        zipcode_no = request.GET['zipcode']
+        check = None
+        data = Zipcode.objects.filter(zipcode=zipcode_no)
+        if len(data) > 0:
+            check = 'in'
+        else:
+            check = 'out'
+
+        return HttpResponse(check, content_type="text/plain")
+
+
 def cbsa_from_zipcode(request):
     if request.method == "GET":
         zipcode_no = request.GET['zipcode']
         cbsa = Zipcode.objects.values("cbsa").filter(zipcode=zipcode_no).values_list('cbsa',flat=True)
-        list_zipcode_from_cbsa = Zipcode.objects.filter(cbsa=cbsa[0])
-        zipcodes = set(zip.zipcode for zip in list_zipcode_from_cbsa)
-        zipcode_boundaries = ZipcodeBoundaryPoint.objects.filter(zipcode__in=zipcodes)
 
-        json_data = None
-        if len(zipcode_boundaries) > 0:
-              json_data = serializers.serialize('json', zipcode_boundaries)
+        if len(cbsa) > 0:
+            list_zipcode_from_cbsa = Zipcode.objects.filter(cbsa=cbsa[0])
+            zipcodes = set(zip.zipcode for zip in list_zipcode_from_cbsa)
+            zipcode_boundaries = ZipcodeBoundaryPoint.objects.filter(zipcode__in=zipcodes)
 
+            json_data = None
+            if len(zipcode_boundaries) > 0:
+                  json_data = serializers.serialize('json', zipcode_boundaries)
+
+        else:
+            json_data = ''
+        
         return HttpResponse(json_data, content_type="application/json")
 
 def zipcode_info(request):
@@ -40,13 +59,9 @@ def zipcode_info(request):
         zipcode_no = request.GET['zipcode']
         zipcode = Zipcode.objects.filter(zipcode=zipcode_no)
 
-        print zipcode
-
         json_data = None
         if len(zipcode) > 0:
             json_data = serializers.serialize('json', zipcode)
-
-        print json_data
 
         return HttpResponse(json_data, content_type="application/json")
 
@@ -58,7 +73,5 @@ def bounds_info(request):
     json_data = None
     if len(points) > 0:
       json_data = serializers.serialize('json', points)
-
-    print json_data
 
     return HttpResponse(json_data, content_type="application/json")
